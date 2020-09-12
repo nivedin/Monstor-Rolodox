f5850ea155670a0b01f26a207a35ea8698153138
Nivedin <nivedinp@gmail.com>
2020-09-12 23:22:52 +0530

    Updates

---
+++ precache-manifest.9109b8564eafaa55b81575e41de6c444.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90ac8902231a0c2c4fa5ea8dc83345bf",
+    "url": "/Monstor-Rolodox/index.html"
+  },
+  {
+    "revision": "7ae83fcfb20e4141605b",
+    "url": "/Monstor-Rolodox/static/css/main.a4ca331f.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/Monstor-Rolodox/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monstor-Rolodox/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ae83fcfb20e4141605b",
+    "url": "/Monstor-Rolodox/static/js/main.4965fbac.chunk.js"
+  },
+  {
+    "revision": "24895ec1a8ff7938cb8f",
+    "url": "/Monstor-Rolodox/static/js/runtime-main.3ef802e1.js"
+  }
+]);
